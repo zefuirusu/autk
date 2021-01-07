bfd60bfe745fedb3fd73ecb66ba293f55af1c042
zefuirusu <zefuirusu@qq.com>
2021-01-08 02:26:27 +0800

    separate script

---
+++ financialtk/examples/README.md
@@ -0,0 +1,6 @@
+# Examples for Financial Toolkit
+## An Example for Sampling
+对北京的抽样会报错,那是因为`10028`科目在序时账里没有记录,筛选出来的`DataFrame`异常.
+## Separate Trial Balance
+天下之大无奇不有，第一次遇到被审计单位把所有主体的科目余额表都放到一张sheet的奇葩情况。
+你牛逼，我比你更牛逼，一个python脚本给你分开。
---
+++ financialtk/examples/autoSample/run.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+from autk.zhchart import ChartAccount
+from autk.zhgl import Gele,Acct
+from autk.sample import AuSample
+
+acctlidir1='./acctlidir/bj-acctli.txt'
+acctlidir2='./acctlidir/xa-acctli.txt'
+
+sdir1='./outSample/bj-Multi.xlsx'
+sdir2='./outSample/xa-Multi.xlsx'
+
+cht1=ChartAccount('./inputData/北京余额表.xlsx')
+cht2=ChartAccount('./inputData/西安余额表.xlsx')
+
+gl1=Gele('./inputData/北京序时账-202001-10.xlsx',shtna='bjgl10',title=0)
+gl2=Gele('./inputData/西安序时账-202001-10.xlsx',shtna='xagl10',title=0)
+
+lg1='./logfiles/bj_sam_log.txt'
+lg2='./logfiles/xa_sam_log.txt'
+# start testing:
+def bj_sample():
+    s1=AuSample(gl1,cht1,sdir1,acctli_dir=acctlidir1,logdir=lg1)
+    s1.multiSample()
+    return
+def xa_sample():
+    s2=AuSample(gl2,cht2,sdir2,acctli_dir=acctlidir2,logdir=lg2)
+    s2.multiSample()
+    return
+if __name__=='__main__':
+    import threading
+    t1=threading.Thread(target=bj_sample)
+    t2=threading.Thread(target=xa_sample)
+    t1.start()
+    t2.start()
+    pass
