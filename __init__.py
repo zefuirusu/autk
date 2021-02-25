8971f5381a58d255bb6dda15011c21270e6d9597
zefuirusu <zefuirusu@qq.com>
2021-02-25 15:45:58 +0800

    def mgl()

--- __init__.py
+++ __init__.py
@@ -5,4 +5,4 @@ import sys
 from .financialtk.modules.mortalGL import MGL
 sys.path.append(os.path.abspath(os.curdir))
 def mgl(fpath='',shtna='Sheet1',title=0,glid_index=[]):
-    return MGL(fpath='',shtna='Sheet1',title=0,glid_index=[])
+    return MGL(fpath=fpath,shtna=shtna,title=title,glid_index=glid_index)
