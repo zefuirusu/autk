31f6a278f75fb61b3ca2436db1214656191e17fe
zefuirusu <zefuirusu@qq.com>
2021-03-01 22:27:02 +0800

    modified

--- __init__.py
+++ __init__.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.curdir))
 from .financialtk.modules.mortalGL import MGL
+from .fileAssistant.skjoinxl import JoinExcel
 # from .financialtk.zhtk.logwriter import wtlog
-from .fileAssistant.skjoinxl import JoinExcel,get_time_str
-print('mgl','joinxl','wtlog')
+print('mgl','joinxl','wtlog','get_time_str')
 def mgl(fpath='',shtna='Sheet1',title=0,glid_index=[],auto=False):
     return MGL(fpath=fpath,shtna=shtna,title=title,glid_index=glid_index,auto=auto)
 def joinxl(savedir=os.path.abspath(os.curdir),prefix='',suffix=get_time_str(woc=True)):
@@ -21,3 +21,16 @@ def wtlog(logline,logdir='./log_default.txt'):
         f.write('\n')
     return
 #
+def get_time_str(woc=False):
+    import time
+    time_list=list(map(str,time.localtime()))
+    if len(time_list[1])==1:
+        time_list[1]='0'+time_list[1]
+        pass
+    else:
+        pass
+    if woc == False:
+        timestr='-'.join(['-'.join(time_list[0:3]),'_'.join(time_list[3:6])])
+    else:
+        timestr=''.join(time_list[0:6])
+    return timestr
--- examples/tjoin.py
+++ examples/tjoin.py
@@ -5,9 +5,9 @@
 import os
 # import sys
 # sys.path.append(os.curdir)
+from autk import get_time_str,joinxl
 # from autk.fileAssistant.skjoinxl import get_time_str,TarSheet,MultiRead,JoinExcel
-from skjoinxl import get_time_str,TarSheet,MultiRead,JoinExcel
-# print(JoinExcel.__dict__)
+# from skjoinxl import get_time_str,TarSheet,MultiRead,JoinExcel
 if __name__=='__main__':
     testdir1='./bj1123-2202'
     testdir2='./bj1221-2241'
@@ -26,7 +26,8 @@ if __name__=='__main__':
     tars=list(get_tars(testdirs[1]))
     # print(get_time_str(True))
     print('start_time:',get_time_str())
-    j1=JoinExcel()
+    # j1=JoinExcel()
+    j1=joinxl()
     j1.load_sheets(tars)
     # j2=j1.join_to_sheet()
     j3=j1.join_to_book(split=False,write=False)
