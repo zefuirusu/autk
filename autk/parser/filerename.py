21254db42584cfd717bf49b92b2b8f03d3360348
zefuirusu <zefuirusu@qq.com>
2022-09-19 09:46:10 +0800

    add suffix 9.19

--- autk/parser/filerename.py
+++ autk/parser/filerename.py
@@ -14,10 +14,29 @@ def suffix(suffix_str,filedir):
         path_list.append(
             file_path
         )
+        continue
+    print(path_list)
     def single_rename(file_path):
         filename=file_path.split(os.sep)[-1]
         cur_pure_name=re.sub(r'\..*$','',filename)
-        new_pure_name=''
+        extension_str=re.sub(r'^.*\.','',filename)
+        new_path=os.path.join(
+            os.sep.join(
+                file_path.split(os.sep)[0:-1]
+            ),
+            ''.join([
+                cur_pure_name,
+                '-',suffix_str,
+                '.',extension_str
+            ])
+        )
+        print('current name:',file_path)
+        print('new name',new_path)
+        shutil.move(file_path,new_path)
+        pass
+    for file in path_list:
+        single_rename(file)
+        continue
     pass
 if __name__=='__main__':
     pass
